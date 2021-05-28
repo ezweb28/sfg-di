8c1b8027d7029bdff48dceef0b04947359cb256d
Sun <ezweb28@gmail.com>
2021-05-28 18:10:57 -0400

    CH35 - CH36. Using Qualifiers

--- src/main/java/guru/springframework/sfgdi/controllers/ContructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ContructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -8,7 +9,7 @@ public class ContructorInjectedController {
     private final GreetingService greetingService;
 
     // No need @Autowired. It can be omitted after Spring 4.2
-    public ContructorInjectedController(GreetingService greetingService) {
+    public ContructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
