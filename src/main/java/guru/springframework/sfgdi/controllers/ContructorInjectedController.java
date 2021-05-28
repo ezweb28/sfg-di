8881440ae15dcf57461ea0148fb96fe2b0f857d4
Sun <ezweb28@gmail.com>
2021-05-27 23:55:45 -0400

    CH35. Dependency Injection with Spring Framework

--- src/main/java/guru/springframework/sfgdi/controllers/ContructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ContructorInjectedController.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ContructorInjectedController {
     private final GreetingService greetingService;
 
+    // No need @Autowired. It can be omitted after Spring 4.2
     public ContructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
